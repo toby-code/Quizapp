6236c637e3c48b71aefb1e1af74fcd5c31da4dc8
toby-code <mueller.to133@gmail.com>
2021-02-10 19:19:33 +0100

    Added footer and random question counter

--- script/game.js
+++ script/game.js
@@ -1,8 +1,32 @@
+let count;
+let countRandom = 0;
+let arr = [];
 
-let count = 1;
 
+/**
+ * Generates random numbers and saves it in array 'arr'
+ * Every number is generated only once
+ * The number of generated numbers is equal to the json 'qAndA' length
+ * If all random numbers are taken function 'levelCompleted' will be executed
+ */
+function getRandoms() {
+    if(arr.length < 1) {
+        while(arr.length < qAndA.length){
+            var r = Math.floor(Math.random() * qAndA.length);
+            if(arr.indexOf(r) === -1) arr.push(r);
+        }
+    }
+    count = arr[countRandom];
+    if(countRandom == qAndA.length) {
+        alert('Quiz abgeschlossen!');
+        count = 0;
+    }
+    countRandom++;
+}
 
 function nextQuestion() {
+    getRandoms();
+    document.querySelector('.btn-info').classList.add('d-none');
     document.querySelector('#question').innerHTML = `${qAndA[count].question} ${qAndA[count].country}`;
     document.querySelector('#answerA').innerHTML = `${qAndA[count].answerA}`;
     document.querySelector('#answerB').innerHTML = `${qAndA[count].answerB}`;
@@ -12,10 +36,12 @@ function nextQuestion() {
 
 function answer(a) {
     let chosenAnswer = document.querySelector(`#${a}`);
-    alert(`Du hast ${chosenAnswer.innerHTML} geantwortet`);
     if(chosenAnswer.innerHTML == qAndA[count].correctAnswer) {
-        alert('Richtig!');
+        document.querySelector('.alert-danger').classList.add('d-none');
+        document.querySelector('.alert-success').classList.remove('d-none');
+        document.querySelector('.btn-info').classList.remove('d-none');
     } else {
-        alert('Falsch!');
+        document.querySelector('.alert-success').classList.add('d-none');
+        document.querySelector('.alert-danger').classList.remove('d-none');
     }
 }

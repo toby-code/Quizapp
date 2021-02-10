78c0f9233986e26747879de67704dfeb7c0fb890
toby-code <mueller.to133@gmail.com>
2021-02-10 20:15:36 +0100

    Added javascript for progress bar and commented javascript code

--- script/game.js
+++ script/game.js
@@ -1,13 +1,15 @@
-let count;
 let countRandom = 0;
+let countQuestion = 0;
+let count;
+let progress;
 let arr = [];
 
 
 /**
- * Generates random numbers and saves it in array 'arr'
- * Every number is generated only once
- * The number of generated numbers is equal to the json 'qAndA' length
- * If all random numbers are taken function 'levelCompleted' will be executed
+ * Generates random numbers and saves it in array 'arr'.
+ * Every number is generated only once.
+ * The number of generated numbers is equal to the json 'qAndA' length.
+ * If all random numbers are taken alert will be displayed.
  */
 function getRandoms() {
     if(arr.length < 1) {
@@ -24,9 +26,36 @@ function getRandoms() {
     countRandom++;
 }
 
+/**
+ * Counts questions and displays question.
+ * @function getProgress
+ * @function getRandoms
+ * @function getQandA
+ */
 function nextQuestion() {
+    countQuestion++;
+    getProgress();
     getRandoms();
+    getQandA();
     document.querySelector('.btn-info').classList.add('d-none');
+}
+
+/**
+ * Calculates and displays progress.
+ */
+function getProgress() {
+    let progressBar = document.querySelector('.progress-bar');
+    progress = countQuestion * 100 / qAndA.length;
+    progress = Math.round(progress); 
+    progressBar.style.width = `${progress}%`;
+    progressBar.innerHTML = `${progress}%`;
+
+}
+
+/**
+ * Loads data from json and displays question and possible answers.
+ */
+function getQandA() {
     document.querySelector('#question').innerHTML = `${qAndA[count].question} ${qAndA[count].country}`;
     document.querySelector('#answerA').innerHTML = `${qAndA[count].answerA}`;
     document.querySelector('#answerB').innerHTML = `${qAndA[count].answerB}`;
@@ -34,6 +63,11 @@ function nextQuestion() {
     document.querySelector('#answerD').innerHTML = `${qAndA[count].answerD}`;
 }
 
+/**
+ * Displays alert green if the answer is correct and alert red if the answer is wrong.
+ * Next question button will be displayed if the answer is correct.
+ * @param {string} a 
+ */
 function answer(a) {
     let chosenAnswer = document.querySelector(`#${a}`);
     if(chosenAnswer.innerHTML == qAndA[count].correctAnswer) {
